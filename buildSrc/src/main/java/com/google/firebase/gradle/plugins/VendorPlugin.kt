3e56dfad9636be5e15ade90bec8faa66057086c4
Denver Coneybeare <dconeybe@google.com>
2021-01-27 16:29:32 +0000

    VendorPlugin.kt: Fix Dagger path build error on Windows

--- buildSrc/src/main/java/com/google/firebase/gradle/plugins/VendorPlugin.kt
+++ buildSrc/src/main/java/com/google/firebase/gradle/plugins/VendorPlugin.kt
@@ -88,7 +88,7 @@ class JarJarTransformer(
         val rulesFile = File.createTempFile(parentPackage, ".jarjar")
         rulesFile.printWriter().use {
             for (externalPackageName in packagesToVendor) {
-                it.println("rule $externalPackageName.** $parentPackage.@0")
+                it.println("rule ${externalPackageName.replace('\\', '.')}.** $parentPackage.@0")
             }
         }
         logger.info("The following JarJar configuration will be used:\n ${rulesFile.readText()}")
